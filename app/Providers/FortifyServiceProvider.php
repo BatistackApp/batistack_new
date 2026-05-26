1bd16468f2da22a4ced4a41755f0cef9d8bb3414
maxim <maximemockelyn8@gmail.com>
2026-05-26 20:31:57 +0200

    feat(auth): Rediriger les utilisateurs selon leur rôle après connexion (develop)
    Cette mise à jour introduit une logique de redirection personnalisée
    après la connexion des utilisateurs.
    
    Détails :
    - **Redirection par rôle** : Les utilisateurs sont désormais redirigés
      vers un tableau de bord spécifique (`/core` pour les admins,
      `/customer` pour les tiers, `/employee` pour les employés)
      en fonction de leur rôle défini.
    - **Implémentation Fortify** : La réponse de connexion par défaut de
      Fortify est remplacée par une implémentation personnalisée de
      `LoginResponse` dans le `FortifyServiceProvider`.

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -4,11 +4,14 @@ namespace App\Providers;
 
 use App\Actions\Fortify\CreateNewUser;
 use App\Actions\Fortify\ResetUserPassword;
+use App\Models\User;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
+use Laravel\Fortify\Contracts\LoginResponse;
 use Laravel\Fortify\Fortify;
 
 class FortifyServiceProvider extends ServiceProvider
@@ -18,7 +21,13 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->instance(LoginResponse::class, new class implements LoginResponse{
+            public function toResponse($request) {
+                if ($request->user()->is_admin) return redirect('/core');
+                if ($request->user()->is_tiers) return redirect('/customer');
+                if ($request->user()->is_employee) return redirect('/employee');
+            }
+        });
     }
 
     /**
