9e5fa95d98316ff84ef154ae2d57c55714a28626
Mockelyn Maxime <contact@vortechstudio.fr>
2026-07-09 17:23:02 +0200

    fix: mon-profil form actions component

--- resources/views/filament/salarie/pages/mon-profil.blade.php
+++ resources/views/filament/salarie/pages/mon-profil.blade.php
@@ -2,28 +2,22 @@
     <x-filament-panels::form wire:submit="saveEmployeeData">
         {{ $this->employeeForm }}
 
-        <x-filament-panels::form.actions
-            :actions="[
-                \Filament\Actions\Action::make('saveEmployeeData')
-                    ->label('Enregistrer les modifications')
-                    ->submit('saveEmployeeData'),
-            ]"
-        />
+        <div class="mt-4">
+            <x-filament::button type="submit">
+                Enregistrer les modifications
+            </x-filament::button>
+        </div>
     </x-filament-panels::form>
 
     <x-filament::section>
         <x-filament-panels::form wire:submit="savePasswordData">
             {{ $this->passwordForm }}
 
-            <x-filament-panels::form.actions
-                :actions="[
-                    \Filament\Actions\Action::make('savePasswordData')
-                        ->label('Changer le mot de passe')
-                        ->submit('savePasswordData')
-                        ->color('warning'),
-                ]"
-                class="mt-4"
-            />
+            <div class="mt-4">
+                <x-filament::button type="submit" color="warning">
+                    Changer le mot de passe
+                </x-filament::button>
+            </div>
         </x-filament-panels::form>
     </x-filament::section>
 </x-filament-panels::page>
